ce3ede62b6863c1d07d0cd95c420029f466f974a
victoricoma <victor.icoma@etec.sp.gov.br>
2025-11-18 20:57:21 -0300

    Aula1118 - Json File System

--- js/action.js
+++ js/action.js
@@ -1,25 +1,36 @@
-function loginSite(){
+async function loginSite() {
+    const dados = await fetch('../data/users.json')
+    const usuarios = await dados.json()
+    
     var email = document.getElementById('email').value
     var senha = document.getElementById('senha').value
 
-    if(email === 'aluno@etec.br' && senha === '1234'){
-        window.confirm("Parabéns você foi Logado com Sucesso!")
+    const user = usuarios.find(u => u.email === email && u.senha === senha)
+
+    if (user) {
+        window.confirm("Bem Vindo: "+ user.nome)
         limparCampos()
-    }else{
-        window.alert("As informações contém Erros!")
+    } else {
+        window.confirm("Verifique sua informações!")
     }
 }
 
-function limparCampos(){
+function limparCampos() {
     document.getElementById('email').value = ""
     document.getElementById('senha').value = ""
 }
 
-function loadRegistro(){
+function loadRegistro() {
     var email = document.getElementById('email').value
     var senha = document.getElementById('senha').value
 
     localStorage.setItem('email', email)
     localStorage.setItem('senha', senha)
     window.location.href = 'registro.html'
+}
+
+async function  exibirUsuarios() {
+    const dados = await fetch("../data/users.json")
+    const usuarios = dados.json()
+    console.table(usuarios)
 }
